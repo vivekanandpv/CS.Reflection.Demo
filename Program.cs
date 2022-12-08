03c15183ddfc9441e5b91434bceeeb5ce85f6859
Vivekanand P V <vivekanandpv@outlook.com>
2022-12-08 12:52:18 +0530

    done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
 
 namespace CS.Reflection.Demo
 {
+    class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //  static or compile-time
+            Type compileTimeTypeInfo = typeof(Car);
+
+            //  runtime
+            var car = new Car();
+            Type runtimeTypeInfo = car.GetType();
+
+            //  current executing type
+            Type currentType = Assembly.GetExecutingAssembly().GetType("CS.Reflection.Demo.Program");
+
+            //  arbitrary type information
+            Type intTypeInfo = Type.GetType("System.Int32, System.Private.CoreLib");
+
+            //  generic type
+            Type genericDictionaryTypeInfo = typeof(Dictionary<,>);
+            Type genericListTypeInfo = typeof(List<>);
+
+            //  array type
+            Type arrayTypeInfo = typeof(int[]);
+
+            Type typeTypeInfo = typeof(Type);
         }
     }
 }
